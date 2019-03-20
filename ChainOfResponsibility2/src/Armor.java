9026325924d9f18b34c578d0d9dd53103eb689a6
khs <hyosub19@gmail.com>
2019-03-20 10:18:59 +0900

    책임사슬 패턴2

---
+++ ChainOfResponsibility2/src/Armor.java
@@ -0,0 +1,40 @@
+
+public class Armor implements Defense {
+
+	private Defense nextDefense;
+	private int def;
+	
+	public Armor() {
+		
+	}
+	
+	public Armor(int def) {
+		this.def = def;
+	}
+	
+	public void setDef(int def) {
+		this.def = def;
+	}
+	
+	@Override
+	public void defense(Attack attack) {
+		
+		/* Point */
+		process(attack);
+		
+		if(nextDefense != null) {
+			nextDefense.defense(attack);
+		}
+	}
+	
+	private void process(Attack attack) {
+		int amount = attack.getAmount();
+		amount -= def;
+		attack.setAmount(amount);
+	}
+	
+	public void setNextDefense(Defense nextDefense) {
+		this.nextDefense = nextDefense;
+	}
+
+}
