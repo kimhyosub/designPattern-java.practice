6433d5f98e4ff9bc7d073dbb80520813fc32ff1e
khs <hyosub19@gmail.com>
2019-03-20 10:00:01 +0900

    책임사슬 패턴

---
+++ ChainOfResponsibility/src/Calculator.java
@@ -0,0 +1,19 @@
+public abstract class Calculator {
+	
+	private Calculator preCalculator;
+
+	public void setNextCalculator(Calculator nextCalculator) {
+		this.preCalculator = nextCalculator;
+	}
+
+	public boolean process(Request request) {
+		
+		if(preCalculator != null)
+			if(preCalculator.process(request))
+				return true;
+		
+		return operator(request);
+	}
+	
+	abstract protected boolean operator(Request request);
+}
