a404a957739094b0dbd10bf005ca4b114cd580f7
khs <hyosub19@gmail.com>
2019-03-20 11:34:50 +0900

    옵저버 패턴

---
+++ Observer/src/com/observer/Application.java
@@ -0,0 +1,20 @@
+package com.observer;
+import com.observer.Button.OnClickListener;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Button button = new Button();
+		
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(Button button) {
+				System.out.println(button + "is Clicked");
+				
+			}
+		});
+		button.onClick();
+	}
+
+}
